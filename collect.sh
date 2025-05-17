2e80a2fb0e0288e49084b79141ae092b7b86e90b
ricky.ediga@gmail.com <ricky.ediga@Rithwiks-Laptop.local>
2025-05-17 13:10:19 -0700

    added new data

---
+++ collect.sh
@@ -0,0 +1,13 @@
+set -e
+cd ~/Desktop/github/rivals
+uv run data_collector.py
+cd ~/Desktop/github/marvel-rivals
+git add .
+
+# Check if there are any changes staged for commit
+if ! git diff --cached --quiet; then
+    git commit -m "added new data"
+    git push origin HEAD
+else
+    echo "No changes to commit."
+fi
